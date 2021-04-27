a848e56d17756fb9498c2c86ec9a03ec6498851d
Piyush <piyushgargofficial@gmail.com>
2021-04-27 18:01:08 +0530

    Footer Completed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,12 +8,17 @@ import RepeatIcon from "@material-ui/icons/Repeat";
 import VolumeDownIcon from "@material-ui/icons/VolumeDown";
 import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
 import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import { Grid, Slider } from "@material-ui/core";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footer__left">
-        <p>Album and Song Details </p>
+        <img src="" alt="" />
+        <div className="footer__songInfo">
+          <h4>Yeah!</h4>
+          <p>Usher</p>
+        </div>
       </div>
       <div className="footer__center">
         <ShuffleIcon className="footer__green" />
@@ -23,7 +28,18 @@ function Footer() {
         <RepeatIcon className="footer__green" />
       </div>
       <div className="footer__right">
-        <p>Volume Controls</p>
+        {/* Material UI Code */}
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider" />
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
